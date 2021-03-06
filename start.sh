1610f586efa2d035ef0032a7b194f54e703c3cec
Sean Droke <drokesean@gmail.com>
2021-03-06 21:36:25 +0000

    Refining message

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
  npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/FF04fSu_p_Oayz25utSyXshVVfdtlXtc &
  sleep 10
  npx hardhat run --network localhost scripts/deploy/0001-PremiaV1.ts
- tail -F anything
+ tail -F message
