009634e0b4b34eed5529106391349a876ca69d73
Sean Droke <drokesean@gmail.com>
2021-03-07 19:49:52 +0000

    Specify block

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/FF04fSu_p_Oayz25utSyXshVVfdtlXtc &
+ npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/FF04fSu_p_Oayz25utSyXshVVfdtlXtc --fork-block-number 11988984 &
  sleep 10
  npx hardhat run --network localhost scripts/deploy/0001-PremiaV1.ts
  tail -F message
